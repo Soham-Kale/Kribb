a603113129618d538eae339b5a0e9eea30b41eba
Soham-Kale <sohamkale3014@gmail.com>
2026-05-12 12:54:03 +0530

    add authentication and tab navigation components

---
+++ app/(root)/(tabs)/saved.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SavedScreen = () => {
+    return (
+        <div>Saved</div>
+    )
+}
+
+export default SavedScreen;
---
+++ app/(root)/(tabs)/search.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchScreen = () => {
+    return (
+        <div>Search</div>
+    )
+}
+
+export default SearchScreen;
