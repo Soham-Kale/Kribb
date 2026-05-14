fc7cfaadfca0a9924f176e91e4d03c46fc615849
Soham-Kale <sohamkale3014@gmail.com>
2026-05-14 11:33:25 +0530

    feat: implement sign-out functionality and add SafeAreaView to TabLayout

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,25 +1,48 @@
+import { useAuth } from '@clerk/expo';
+import { router } from 'expo-router';
 import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
-    return (
-        <NativeTabs>
-        <NativeTabs.Trigger name="index">
-            <Label>Home</Label>
-            <Icon sf="house.fill" />
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="search">
-            <Icon sf="magnifyingglass" />
-            <Label>Search</Label>
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="saved">
-            <Icon sf="bookmark.fill" />
-            <Label>Saved</Label>
-        </NativeTabs.Trigger>
 
-        <NativeTabs.Trigger name="profile">
-            <Icon sf="person.circle" />
-            <Label>Profile</Label>
-        </NativeTabs.Trigger>
-        </NativeTabs>
+    const { signOut } = useAuth();
+
+    const onPressSignOut = async () => {
+        try{
+            await signOut();
+            router.replace("/sign-in");
+        }catch(error) {
+            alert("Error signing out: " + error);
+        }
+    };
+
+    return (
+        // <NativeTabs>
+        // <NativeTabs.Trigger name="index">
+        //     <Label>Home</Label>
+        //     <Icon sf="house.fill" />
+        // </NativeTabs.Trigger>
+        // <NativeTabs.Trigger name="search">
+        //     <Icon sf="magnifyingglass" />
+        //     <Label>Search</Label>
+        // </NativeTabs.Trigger>
+        // <NativeTabs.Trigger name="saved">
+        //     <Icon sf="bookmark.fill" />
+        //     <Label>Saved</Label>
+        // </NativeTabs.Trigger>
+
+        // <NativeTabs.Trigger name="profile">
+        //     <Icon sf="person.circle" />
+        //     <Label>Profile</Label>
+        // </NativeTabs.Trigger>
+        // </NativeTabs>
+        
+        <SafeAreaView className="flex-1 items-center justify-center">
+            <Text>Home</Text>
+            <TouchableOpacity onPress={onPressSignOut}>
+                <Text>Sign Out</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
