3f99c7823a9a1bf9da56cda2d331ee6b3ef79362
Soham-Kale <sohamkale3014@gmail.com>
2026-05-24 19:51:59 +0530

    refactor Create component layout for improved styling

--- app/(root)/(tabs)/create.tsx
+++ app/(root)/(tabs)/create.tsx
@@ -4,8 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Create() {
     return (
-        <SafeAreaView className='flex-1'>
-            <View>
+        <SafeAreaView className='flex-1' >
+            <View className='flex-1 align-center justify-content' >
                 <Text>Create</Text>
             </View>
         </SafeAreaView>
